d63c36e2227628cbbbeda29cf80657c67196e810
liujuanjuan <liujuanjuan134@foxmail.com>
2020-08-11 21:19:28 +0800

    添加导航布局

--- view/src/components/pages/ShoppingMall.vue
+++ view/src/components/pages/ShoppingMall.vue
@@ -1,21 +1,29 @@
 <template>
-  <van-row>
-    <van-col span="8">span: 8</van-col>
-    <van-col span="8">span: 8</van-col>
-    <van-col span="8">span: 8</van-col>
-</van-row>
+  <!--search bar layout-->
+  <div class="search-bar">
+    <van-row>
+      <van-col span="3"><img :src="locationIcon" width="100%" /></van-col>
+      <van-col span="16">serach input</van-col>
+      <van-col span="5">button</van-col>
+    </van-row>
+  </div>
 </template>
 
 <script>
 export default {
   data: function () {
     return {
-      message: 'Message'
+      message: 'Message',
+      locationIcon: require('../../assets/images/location.png')
     }
   }
 }
 </script>
 
 <style>
-
+.search-bar{
+  height: 2.2rem;
+  background-color: #d8b338;
+  line-height:2.2rem;
+}
 </style>
